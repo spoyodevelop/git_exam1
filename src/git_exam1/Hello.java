6958e886e472a3231f4eda91d1103781440da7bc
spoyodevelop <kjy9505@gmail.com>
2021-09-28 18:54:12 +0900

    201641007_4

---
+++ src/git_exam1/Hello.java
@@ -0,0 +1,11 @@
+package git_exam1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("소속: 미디어컨텐츠융합자율학부");
+		System.out.println("학번: 201641007");
+		System.out.println("이름: 김재영");
+	}
+
+}
